48d782b982345a866bbe4d80c28cbb5b0a9b1438
anton startsev <anton.startsev@osome.com>
2022-09-19 15:35:00 +0300

    login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import logo from '../../images/logo.svg';
+import { Link } from 'react-router-dom';
 
 function Login() {
     return (
-        <div>
-            <p>Login</p>
-        </div>
+        <main className='login'>
+            <section className='auth'>
+                <img src={logo} alt='логотип' className='auth__logo header__logo'/>
+                <h1 className='auth__title'>Рады видеть!</h1>
+                <form className='auth__form'>
+                    <label className='auth__input-container'>
+                        <span className='auth__input-label'>E-mail</span>
+                        <input
+                            type='email'
+                            name='e-mail'
+                            placeholder='pochta@yandex.ru'
+                            className='auth__input'
+                            minLength='2'
+                            maxLength='30'
+                            required
+                            />
+                    </label>
+                    <label className='auth__input-container'>
+                        <span className='auth__input-label'>Пароль</span>
+                        <input
+                            type='password'
+                            name='password'
+                            placeholder='123'
+                            className='auth__input'
+                            minLength='2'
+                            maxLength='30'
+                            required
+                            />
+                    </label>
+                    <button className='auth__submit login__submit' type='submit'>Войти</button>
+                    <div className='auth__login-link-container'>
+                        <p className='auth__login-link-desc'>Еще не зарегистрированы?</p>
+                        <Link to='/signup' className='auth__login-link'>Регистрация</Link>
+                    </div>
+                </form>
+            </section>
+        </main>
     )
 }
 
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -5,51 +5,51 @@ import { Link } from 'react-router-dom';
 function Register() {
     return (
         <main>
-            <section className='register'>
-                <img src={logo} alt='логотип' className='register__logo header__logo'/>
-                <h1 className='register__title'>Добро пожаловать!</h1>
-                <form className='register__form'>
-                    <label className='register__input-container'>
-                        <span className='register__input-label'>Имя</span>
+            <section className='auth'>
+                <img src={logo} alt='логотип' className='auth__logo header__logo'/>
+                <h1 className='auth__title'>Добро пожаловать!</h1>
+                <form className='auth__form'>
+                    <label className='auth__input-container'>
+                        <span className='auth__input-label'>Имя</span>
                         <input
                             type='text'
                             name='text'
                             placeholder='Виталий'
-                            className='register__input'
+                            className='auth__input'
                             minLength='2'
                             maxLength='30'
                             required
                             />
                     </label>
-                    <label className='register__input-container'>
-                        <span className='register__input-label'>E-mail</span>
+                    <label className='auth__input-container'>
+                        <span className='auth__input-label'>E-mail</span>
                         <input
                             type='email'
                             name='e-mail'
                             placeholder='pochta@yandex.ru'
-                            className='register__input'
+                            className='auth__input'
                             minLength='2'
                             maxLength='30'
                             required
                             />
                     </label>
-                    <label className='register__input-container'>
-                        <span className='register__input-label'>Пароль</span>
+                    <label className='auth__input-container'>
+                        <span className='auth__input-label'>Пароль</span>
                         <input
                             type='password'
                             name='password'
                             placeholder='123'
-                            className='register__input'
+                            className='auth__input'
                             minLength='2'
                             maxLength='30'
                             required
                             />
                     </label>
-                    <span className='register__error'>Что-то пошло не так...</span>
-                    <button className='register__submit' type='submit'>Зарегистрироваться</button>
-                    <div className='register__login-link-container'>
-                        <p className='register__login-link-desc'>Уже зарегистрированы?</p>
-                        <Link to='/signin' className='register__login-link'>Войти</Link>
+                    <span className='auth__error'>Что-то пошло не так...</span>
+                    <button className='auth__submit' type='submit'>Зарегистрироваться</button>
+                    <div className='auth__login-link-container'>
+                        <p className='auth__login-link-desc'>Уже зарегистрированы?</p>
+                        <Link to='/signin' className='auth__login-link'>Войти</Link>
                     </div>
                 </form>
             </section>
