7ad6a089ee716c3a77cc98aaa40877ec70eb6da0
anton startsev <anton.startsev@osome.com>
2022-10-09 21:11:11 +0300

    url

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -1,5 +1,5 @@
 // const BASE_URL = 'http://localhost:3000';
-const BASE_URL = 'https://api.movie.nomorepartiesxyz.ru/';
+const BASE_URL = 'https://api.movie.nomorepartiesxyz.ru';
 
 export const register = (name, email, password) => {
     return fetch(`${BASE_URL}/signup`, {
