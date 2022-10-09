69288b04f5df69caf700fb748accc061f30b7093
anton startsev <anton.startsev@osome.com>
2022-10-09 15:49:15 +0300

    add remote

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -77,7 +77,8 @@ class MainApi {
 }
 
 export const mainApi = new MainApi({
-    url: 'http://localhost:3000/',
+    // url: 'http://localhost:3000/',
+    url: 'https://api.movie.nomorepartiesxyz.ru/',
     headers: {
         'content-type': 'application/json',
     }
